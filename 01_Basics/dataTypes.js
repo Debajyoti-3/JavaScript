7a9cef7297e6d3ec8a98e7d4371be31aba951525
Debajyoti Mitra <168395271+Debajyoti-3@users.noreply.github.com>
2024-11-03 10:34:46 +0000

    Done

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using NodeJS not browser
+
+
+
+
+
+
+
+
+let name ="rick"
+let age = 17
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undifined => value not assigned
+// symbol => unique
+
+
+// Object
+console.log(typeof "Rick");  // undifined
+console.log(typeof null);   // object 
+
